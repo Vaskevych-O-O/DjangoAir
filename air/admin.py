4182281d50c26e12a3689b25d98029b3485ce681
Oleh Vaskevyh <oleh.vaskevych@gmail.com>
2025-07-08 15:21:52 +0300

    FIX: Fixed code duplication in the admin module

--- air/admin.py
+++ air/admin.py
@@ -4,202 +4,50 @@ from .models import (AirlineUser, Airplane, Baggage, BoardingPass, CheckIn,
                      Comfort, DietaryOption, Flight, Meal, Ticket)
 
 
-class TicketAdmin(admin.ModelAdmin):
+class RoleBasedAdmin(admin.ModelAdmin):
+    allowed_roles = []
+
     def has_module_permission(self, request):
-        if not request.user.is_authenticated:
-            return False
-        return request.user.role in ["supervisor", "checkin_manager", "gate_manager"]
+        return request.user.is_authenticated and request.user.role in self.allowed_roles
 
     def has_view_permission(self, request, obj=None):
         if not request.user.is_authenticated:
             return False
-        if request.user.role in ["supervisor", "checkin_manager", "gate_manager"]:
+        if request.user.role in self.allowed_roles:
             return True
         return (
             obj is not None
             and request.user.role == "passenger"
-            and obj.passenger == request.user
+            and getattr(obj, "passenger", None) == request.user
         )
 
     def has_change_permission(self, request, obj=None):
-        if not request.user.is_authenticated:
-            return False
-        return request.user.role == "supervisor"
+        return request.user.is_authenticated and request.user.role in self.allowed_roles
 
     def has_add_permission(self, request):
-        if not request.user.is_authenticated:
-            return False
-        return request.user.role == "supervisor"
+        return request.user.is_authenticated and request.user.role in self.allowed_roles
 
     def has_delete_permission(self, request, obj=None):
-        if not request.user.is_authenticated:
-            return False
-        return request.user.role == "supervisor"
+        return request.user.is_authenticated and request.user.role in self.allowed_roles
 
 
-class CheckInManager(admin.ModelAdmin):
-    def has_module_permission(self, request):
-        if not request.user.is_authenticated:
-            return False
-        return request.user.role in ["checkin_manager", "supervisor"]
+class TicketAdmin(RoleBasedAdmin):
+    allowed_roles = ["supervisor", "checkin_manager", "gate_manager"]
 
-    def has_view_permission(self, request, obj=None):
-        if not request.user.is_authenticated:
-            return False
-        if request.user.role in ["supervisor", "checkin_manager"]:
-            return True
-        return (
-            obj is not None
-            and request.user.role == "passenger"
-            and obj.passenger == request.user
-        )
+class CheckInManager(RoleBasedAdmin):
+    allowed_roles = ["supervisor", "checkin_manager"]
 
-    def has_change_permission(self, request, obj=None):
-        if not request.user.is_authenticated:
-            return False
-        return request.user.role in ["supervisor", "checkin_manager"]
+class GateManager(RoleBasedAdmin):
+    allowed_roles = ["supervisor", "gate_manager"]
 
-    def has_add_permission(self, request):
-        if not request.user.is_authenticated:
-            return False
-        return request.user.role in ["supervisor", "checkin_manager"]
+class AirlineUserAdmin(RoleBasedAdmin):
+    allowed_roles = ["supervisor"]
 
-    def has_delete_permission(self, request, obj=None):
-        if not request.user.is_authenticated:
-            return False
-        return request.user.role in ["supervisor", "checkin_manager"]
+class AirplaneAdmin(RoleBasedAdmin):
+    allowed_roles = ["supervisor"]
 
-
-class GateManager(admin.ModelAdmin):
-    def has_module_permission(self, request):
-        if not request.user.is_authenticated:
-            return False
-        return request.user.role in ["gate_manager", "supervisor"]
-
-    def has_view_permission(self, request, obj=None):
-        if not request.user.is_authenticated:
-            return False
-        if request.user.role in ["supervisor", "gate_manager"]:
-            return True
-        return (
-            obj is not None
-            and request.user.role == "passenger"
-            and obj.passenger == request.user
-        )
-
-    def has_change_permission(self, request, obj=None):
-        if not request.user.is_authenticated:
-            return False
-        return request.user.role in ["supervisor", "gate_manager"]
-
-    def has_add_permission(self, request):
-        if not request.user.is_authenticated:
-            return False
-        return request.user.role in ["supervisor", "gate_manager"]
-
-    def has_delete_permission(self, request, obj=None):
-        if not request.user.is_authenticated:
-            return False
-        return request.user.role in ["supervisor", "gate_manager"]
-
-
-class AirlineUserAdmin(admin.ModelAdmin):
-    def has_module_permission(self, request):
-        if not request.user.is_authenticated:
-            return False
-        return request.user.role in ["supervisor"]
-
-    def has_view_permission(self, request, obj=None):
-        if not request.user.is_authenticated:
-            return False
-        if request.user.role in ["supervisor"]:
-            return True
-        return (
-            obj is not None
-            and request.user.role == "passenger"
-            and obj.passenger == request.user
-        )
-
-    def has_change_permission(self, request, obj=None):
-        if not request.user.is_authenticated:
-            return False
-        return request.user.role in ["supervisor"]
-
-    def has_add_permission(self, request):
-        if not request.user.is_authenticated:
-            return False
-        return request.user.role in ["supervisor"]
-
-    def has_delete_permission(self, request, obj=None):
-        if not request.user.is_authenticated:
-            return False
-        return request.user.role in ["supervisor"]
-
-
-class AirplaneAdmin(admin.ModelAdmin):
-    def has_module_permission(self, request):
-        if not request.user.is_authenticated:
-            return False
-        return request.user.role in ["supervisor"]
-
-    def has_view_permission(self, request, obj=None):
-        if not request.user.is_authenticated:
-            return False
-        if request.user.role in ["supervisor"]:
-            return True
-        return (
-            obj is not None
-            and request.user.role == "passenger"
-            and obj.passenger == request.user
-        )
-
-    def has_change_permission(self, request, obj=None):
-        if not request.user.is_authenticated:
-            return False
-        return request.user.role in ["supervisor"]
-
-    def has_add_permission(self, request):
-        if not request.user.is_authenticated:
-            return False
-        return request.user.role in ["supervisor"]
-
-    def has_delete_permission(self, request, obj=None):
-        if not request.user.is_authenticated:
-            return False
-        return request.user.role in ["supervisor"]
-
-
-class FlightAdmin(admin.ModelAdmin):
-    def has_module_permission(self, request):
-        if not request.user.is_authenticated:
-            return False
-        return request.user.role in ["supervisor"]
-
-    def has_view_permission(self, request, obj=None):
-        if not request.user.is_authenticated:
-            return False
-        if request.user.role in ["supervisor"]:
-            return True
-        return (
-            obj is not None
-            and request.user.role == "passenger"
-            and obj.passenger == request.user
-        )
-
-    def has_change_permission(self, request, obj=None):
-        if not request.user.is_authenticated:
-            return False
-        return request.user.role in ["supervisor"]
-
-    def has_add_permission(self, request):
-        if not request.user.is_authenticated:
-            return False
-        return request.user.role in ["supervisor"]
-
-    def has_delete_permission(self, request, obj=None):
-        if not request.user.is_authenticated:
-            return False
-        return request.user.role in ["supervisor"]
+class FlightAdmin(RoleBasedAdmin):
+    allowed_roles = ["supervisor"]
 
 
 admin.site.register(AirlineUser, AirlineUserAdmin)
