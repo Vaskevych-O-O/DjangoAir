eff54a2ce03f2c553ae24eb3981c7aeb1eda0f9f
Oleh Vaskevyh <oleh.vaskevych@gmail.com>
2025-07-08 15:15:08 +0300

    FIX: ASGI settings updated

--- DjangoAir/asgi.py
+++ DjangoAir/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DjangoAir.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", os.getenv("DJANGO_ENV", "DjangoAir.settings.dev"))
 
 application = get_asgi_application()
